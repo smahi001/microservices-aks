60977cb5e29dd1cc37f3ae022c1abb6c1cd2eb06
smahi001 <08monisha19962gmail.com>
2026-03-22 18:53:55 -0400

    microservices initial

---
+++ product-service/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("User Service Running");
+});
+
+app.listen(3000, () => console.log("User service on 3000"));
